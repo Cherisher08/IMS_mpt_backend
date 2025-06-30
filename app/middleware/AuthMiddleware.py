5d4e89ea85193617ff5d07bc0671786c350c0613
Cherisher08 <vigu2002ram@gmail.com>
2025-06-30 23:14:14 +0530

    Add middle ware for security

--- app/middleware/AuthMiddleware.py
+++ app/middleware/AuthMiddleware.py
@@ -7,32 +7,32 @@ from app.config import env
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        # if request.url.path in [
-        #     "/",
-        #     "/docs",
-        #     "/favicon.ico",
-        #     "/openapi.json",
-        # ] or request.url.path.startswith("/auth/users"):
-        #     return await call_next(request)
+        if request.url.path in [
+            "/",
+            "/docs",
+            "/favicon.ico",
+            "/openapi.json",
+        ] or request.url.path.startswith("/auth/users"):
+            return await call_next(request)
 
-        # auth_header = request.headers.get("Authorization")
-        # if not auth_header or not auth_header.startswith("Bearer "):
-        #     return JSONResponse(
-        #         status_code=401,
-        #         content={"detail": "Unauthorized"},
-        #     )
+        auth_header = request.headers.get("Authorization")
+        if not auth_header or not auth_header.startswith("Bearer "):
+            return JSONResponse(
+                status_code=401,
+                content={"detail": "Unauthorized"},
+            )
 
-        # token = auth_header.split("Bearer ")[1]
+        token = auth_header.split("Bearer ")[1]
 
-        # try:
-        #     jwt.decode(token, env.secret_key, algorithms="HS256")
-        # except JWTError:
-        #     return JSONResponse(
-        #         status_code=401,
-        #         content={
-        #             "detail": " Access Token is Expired or Invalid. Please Login again"
-        #         },
-        #     )
+        try:
+            jwt.decode(token, env.secret_key, algorithms="HS256")
+        except JWTError:
+            return JSONResponse(
+                status_code=401,
+                content={
+                    "detail": " Access Token is Expired or Invalid. Please Login again"
+                },
+            )
 
         response = await call_next(request)
         return response
