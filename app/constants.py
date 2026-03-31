6ebc1d0849eda11b69da4b7ab09a195506646ca3
Cherisher08 <vigu2002ram@gmail.com>
2026-03-31 22:26:57 +0530

    refactor: :recycle: Update shop name

--- app/constants.py
+++ app/constants.py
@@ -8,7 +8,7 @@ Your One-Time Password (OTP) is: {otp}
 This OTP will expire in 15 minutes. Please do not share it with anyone.
 
 Best regards,  
-Mani Power Tools
+Mani Construction Equipments
 """
 
 ID = "_id"
